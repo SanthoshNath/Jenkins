ffe5275bc9af53d18839336b9fb09c2a09bfb64c
Santhosh <hsohtnas001@gmail.com>
2022-09-04 18:35:39 +0530

    Output jenkins instance's public dns

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "jenkins_instance_public_dns" {
+  value = aws_instance.jenkins_instance.public_dns
+}
+
 output "jenkins_url" {
   value = "http://${aws_instance.jenkins_instance.public_dns}:${var.jenkins_port}"
 }
